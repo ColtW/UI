cd7eefce292287e7a74f489828b5995374986a5c
monkey <monkey@BB21007>
2016-12-08 15:23:21 -0600

    Added login functionality

--- TeamBWindowsFormLoginPage/TeamBWindowsFormLoginPage/Form1.Designer.cs
+++ TeamBWindowsFormLoginPage/TeamBWindowsFormLoginPage/Form1.Designer.cs
@@ -112,6 +112,7 @@
             this.button1.TabIndex = 6;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -147,6 +148,7 @@
             this.button3.TabIndex = 9;
             this.button3.Text = "Check Available Ground Floor Spaces";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // LoginPage
             // 
