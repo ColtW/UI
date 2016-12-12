48c8e3b68f992d1cf5ef87319bb0eb080fb82828
monkey <monkey@BB21007>
2016-12-12 14:27:58 -0600

    working on reservations still

--- TeamBWindowsFormManagerPage/TeamBWindowsFormManagerPage/Form1.Designer.cs
+++ TeamBWindowsFormManagerPage/TeamBWindowsFormManagerPage/Form1.Designer.cs
@@ -49,12 +49,10 @@
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.textBox8 = new System.Windows.Forms.TextBox();
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.textBox10 = new System.Windows.Forms.TextBox();
-            this.textBox11 = new System.Windows.Forms.TextBox();
             this.textBox12 = new System.Windows.Forms.TextBox();
             this.button6 = new System.Windows.Forms.Button();
             this.label14 = new System.Windows.Forms.Label();
@@ -100,12 +98,12 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(8, 223);
+            this.label2.Location = new System.Drawing.Point(11, 203);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 17);
+            this.label2.Size = new System.Drawing.Size(119, 17);
             this.label2.TabIndex = 6;
-            this.label2.Text = "First Name";
+            this.label2.Text = "Confirmation ID";
             // 
             // label3
             // 
@@ -122,12 +120,12 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(182, 223);
+            this.label4.Location = new System.Drawing.Point(265, 206);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(111, 17);
+            this.label4.Size = new System.Drawing.Size(92, 17);
             this.label4.TabIndex = 8;
-            this.label4.Text = "Email Address";
+            this.label4.Text = "Lot Number";
             // 
             // label5
             // 
@@ -176,7 +174,7 @@
             // textBox1
             // 
             this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(98, 222);
+            this.textBox1.Location = new System.Drawing.Point(133, 206);
             this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(76, 20);
@@ -194,10 +192,10 @@
             // textBox3
             // 
             this.textBox3.Enabled = false;
-            this.textBox3.Location = new System.Drawing.Point(308, 225);
+            this.textBox3.Location = new System.Drawing.Point(361, 205);
             this.textBox3.Margin = new System.Windows.Forms.Padding(2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(111, 20);
+            this.textBox3.Size = new System.Drawing.Size(64, 20);
             this.textBox3.TabIndex = 15;
             // 
             // textBox4
@@ -240,19 +238,20 @@
             // 
             this.button5.Enabled = false;
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(115, 371);
+            this.button5.Location = new System.Drawing.Point(121, 414);
             this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(187, 27);
             this.button5.TabIndex = 23;
             this.button5.Text = "Modify Reservation";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(10, 415);
+            this.label9.Location = new System.Drawing.Point(8, 347);
             this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(42, 17);
@@ -263,7 +262,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(130, 417);
+            this.label10.Location = new System.Drawing.Point(134, 347);
             this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(78, 17);
@@ -274,23 +273,12 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(282, 418);
+            this.label11.Location = new System.Drawing.Point(284, 347);
             this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(76, 17);
             this.label11.TabIndex = 26;
             this.label11.Text = "End Time";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(10, 448);
-            this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(252, 17);
-            this.label12.TabIndex = 27;
-            this.label12.Text = "Reservation Confirmation Number";
             // 
             // label13
             // 
@@ -306,7 +294,7 @@
             // textBox8
             // 
             this.textBox8.Enabled = false;
-            this.textBox8.Location = new System.Drawing.Point(51, 415);
+            this.textBox8.Location = new System.Drawing.Point(54, 347);
             this.textBox8.Margin = new System.Windows.Forms.Padding(2);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(76, 20);
@@ -315,7 +303,7 @@
             // textBox9
             // 
             this.textBox9.Enabled = false;
-            this.textBox9.Location = new System.Drawing.Point(212, 418);
+            this.textBox9.Location = new System.Drawing.Point(213, 347);
             this.textBox9.Margin = new System.Windows.Forms.Padding(2);
             this.textBox9.Name = "textBox9";
             this.textBox9.Size = new System.Drawing.Size(67, 20);
@@ -324,20 +312,11 @@
             // textBox10
             // 
             this.textBox10.Enabled = false;
-            this.textBox10.Location = new System.Drawing.Point(358, 418);
+            this.textBox10.Location = new System.Drawing.Point(364, 347);
             this.textBox10.Margin = new System.Windows.Forms.Padding(2);
             this.textBox10.Name = "textBox10";
             this.textBox10.Size = new System.Drawing.Size(67, 20);
             this.textBox10.TabIndex = 31;
-            // 
-            // textBox11
-            // 
-            this.textBox11.Enabled = false;
-            this.textBox11.Location = new System.Drawing.Point(308, 448);
-            this.textBox11.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox11.Name = "textBox11";
-            this.textBox11.Size = new System.Drawing.Size(76, 20);
-            this.textBox11.TabIndex = 32;
             // 
             // textBox12
             // 
@@ -399,12 +378,10 @@
             this.Controls.Add(this.button6);
             this.Controls.Add(this.textBox13);
             this.Controls.Add(this.textBox12);
-            this.Controls.Add(this.textBox11);
             this.Controls.Add(this.textBox10);
             this.Controls.Add(this.textBox9);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.label13);
-            this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
@@ -459,12 +436,10 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.TextBox textBox10;
-        private System.Windows.Forms.TextBox textBox11;
         private System.Windows.Forms.TextBox textBox12;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Label label14;
--- TeamBWindowsFormManagerPage/TeamBWindowsFormManagerPage/Form1.cs
+++ TeamBWindowsFormManagerPage/TeamBWindowsFormManagerPage/Form1.cs
@@ -27,11 +27,24 @@ namespace TeamBWindowsFormManagerPage
 
         private void button1_Click(object sender, EventArgs e)
         {
-            UIdll.CurrentParkingReservation searchReservation = new CurrentParkingReservation();
-            searchReservation.searchReservation();
             
-            textBox1.Text = s
+
+            UIdll.CurrentParkingReservation searchReservation = new CurrentParkingReservation();
+
+            searchReservation.reserveID = textBox13.Text;
+            searchReservation.searchReservation();
+
+            textBox1.Text = searchReservation.confirmation;
+            textBox8.Text = searchReservation.reserveDate;
+            textBox9.Text = searchReservation.start;
+            textBox10.Text = searchReservation.end;
+            textBox3.Text = searchReservation.spaceNumber;
+
         }
 
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
