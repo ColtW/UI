7a25255336532345b1bba2294d459fbd1e3b6f80
Josiah0124864 <josiahemit93@yahoo.com>
2016-12-13 14:38:09 -0600

    added more to the view reservations

--- TeamBWindowsFormViewReservationPage/TeamBWindowsFormViewReservationPage/Form1.Designer.cs
+++ TeamBWindowsFormViewReservationPage/TeamBWindowsFormViewReservationPage/Form1.Designer.cs
@@ -156,6 +156,7 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(393, 64);
             this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // lastName
             // 
