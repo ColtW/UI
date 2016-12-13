fda97eae7836decad9b833dc19eac84cd224d431
Colin <Colin@COLIN-PC>
2016-12-12 21:22:30 -0600

    Colin has started the manager pg 2 there needs to be more work done in the dll for this to be complete because of all of the things in the form that is needed. job desc, pay, etc...

--- TeamBWindowsFormManagerPage2/TeamBWindowsFormManagerPage2/Form1.cs
+++ TeamBWindowsFormManagerPage2/TeamBWindowsFormManagerPage2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UIdll;
 
 namespace TeamBWindowsFormManagerPage2
 {
@@ -20,6 +21,22 @@ namespace TeamBWindowsFormManagerPage2
         private void Form1_Load(object sender, EventArgs e)
         {
             headerBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // I've added only that, but looks like on the employee dll there needs to be more things...
+            // such as last name, date of hire, job, and pay. - Colin
+            UIdll.Employee hireEmployee = new UIdll.Employee();
+            hireEmployee.name = textBox4.Text;
+            
+            
+
         }
     }
 }
