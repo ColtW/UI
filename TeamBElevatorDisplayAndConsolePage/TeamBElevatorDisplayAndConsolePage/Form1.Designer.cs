3552203a0ea3a0a5691f519b4b6ad6cdd8a14ecd
Josiah0124864 <josiahemit93@yahoo.com>
2016-12-14 15:02:57 -0600

    put code into elevator console

--- TeamBElevatorDisplayAndConsolePage/TeamBElevatorDisplayAndConsolePage/Form1.Designer.cs
+++ TeamBElevatorDisplayAndConsolePage/TeamBElevatorDisplayAndConsolePage/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.headerBox = new System.Windows.Forms.PictureBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.button10 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox10 = new System.Windows.Forms.TextBox();
@@ -43,6 +42,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button0 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.toReservations = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.headerBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,31 +56,22 @@
             this.headerBox.TabIndex = 0;
             this.headerBox.TabStop = false;
             // 
-            // listBox1
-            // 
-            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(12, 151);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(508, 104);
-            this.listBox1.TabIndex = 1;
-            // 
             // button10
             // 
             this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button10.Location = new System.Drawing.Point(358, 309);
+            this.button10.Location = new System.Drawing.Point(292, 307);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(87, 38);
             this.button10.TabIndex = 11;
             this.button10.Text = "Submit";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(86, 426);
+            this.label1.Location = new System.Drawing.Point(20, 424);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(208, 20);
             this.label1.TabIndex = 12;
@@ -87,7 +79,7 @@
             // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(312, 426);
+            this.textBox10.Location = new System.Drawing.Point(246, 424);
             this.textBox10.Name = "textBox10";
             this.textBox10.Size = new System.Drawing.Size(50, 22);
             this.textBox10.TabIndex = 13;
@@ -95,7 +87,7 @@
             // button7
             // 
             this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(117, 280);
+            this.button7.Location = new System.Drawing.Point(51, 278);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(51, 33);
             this.button7.TabIndex = 14;
@@ -105,7 +97,7 @@
             // button8
             // 
             this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.Location = new System.Drawing.Point(186, 280);
+            this.button8.Location = new System.Drawing.Point(120, 278);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(51, 33);
             this.button8.TabIndex = 15;
@@ -115,7 +107,7 @@
             // button9
             // 
             this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button9.Location = new System.Drawing.Point(258, 280);
+            this.button9.Location = new System.Drawing.Point(192, 278);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(51, 33);
             this.button9.TabIndex = 16;
@@ -125,7 +117,7 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(117, 329);
+            this.button4.Location = new System.Drawing.Point(51, 327);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(51, 33);
             this.button4.TabIndex = 17;
@@ -135,7 +127,7 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(186, 329);
+            this.button5.Location = new System.Drawing.Point(120, 327);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(51, 33);
             this.button5.TabIndex = 18;
@@ -145,7 +137,7 @@
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(258, 329);
+            this.button6.Location = new System.Drawing.Point(192, 327);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(51, 33);
             this.button6.TabIndex = 19;
@@ -155,7 +147,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(153, 373);
+            this.button1.Location = new System.Drawing.Point(87, 371);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(51, 33);
             this.button1.TabIndex = 20;
@@ -165,7 +157,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(224, 373);
+            this.button2.Location = new System.Drawing.Point(158, 371);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(51, 33);
             this.button2.TabIndex = 21;
@@ -175,7 +167,7 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(295, 373);
+            this.button3.Location = new System.Drawing.Point(229, 371);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(51, 33);
             this.button3.TabIndex = 22;
@@ -185,18 +177,40 @@
             // button0
             // 
             this.button0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button0.Location = new System.Drawing.Point(81, 373);
+            this.button0.Location = new System.Drawing.Point(15, 371);
             this.button0.Name = "button0";
             this.button0.Size = new System.Drawing.Size(51, 33);
             this.button0.TabIndex = 23;
             this.button0.Text = "0";
             this.button0.UseVisualStyleBackColor = true;
             // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(243, 171);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 17);
+            this.lbl1.TabIndex = 24;
+            // 
+            // toReservations
+            // 
+            this.toReservations.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toReservations.Location = new System.Drawing.Point(352, 371);
+            this.toReservations.Name = "toReservations";
+            this.toReservations.Size = new System.Drawing.Size(153, 66);
+            this.toReservations.TabIndex = 25;
+            this.toReservations.Text = "Select to go to Reservations";
+            this.toReservations.UseVisualStyleBackColor = true;
+            this.toReservations.Visible = false;
+            this.toReservations.Click += new System.EventHandler(this.toReservations_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(532, 475);
+            this.Controls.Add(this.toReservations);
+            this.Controls.Add(this.lbl1);
             this.Controls.Add(this.button0);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -210,7 +224,6 @@
             this.Controls.Add(this.textBox10);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button10);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.headerBox);
             this.Name = "Form1";
             this.Text = "ElevatorDisplay and Console";
@@ -224,7 +237,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox headerBox;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox10;
@@ -238,6 +250,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button toReservations;
     }
 }
 
--- TeamBElevatorDisplayAndConsolePage/TeamBElevatorDisplayAndConsolePage/Form1.cs
+++ TeamBElevatorDisplayAndConsolePage/TeamBElevatorDisplayAndConsolePage/Form1.cs
@@ -7,19 +7,144 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using UIdll;
+using TeamBWindowsFormViewReservationPage;
 
 namespace TeamBElevatorDisplayAndConsolePage
 {
     public partial class Form1 : Form
     {
+        public string runningTotal { get; set; }
+        public int CustomerID { get; set; }
+        public int ReservationID { get; set; }
+        public DateTime startTime { get; set; }
+
         public Form1()
         {
             InitializeComponent();
+
+            button1.Click += GetNumericButtonText;
+            button2.Click += GetNumericButtonText;
+            button3.Click += GetNumericButtonText;
+            button4.Click += GetNumericButtonText;
+            button5.Click += GetNumericButtonText;
+            button6.Click += GetNumericButtonText;
+            button7.Click += GetNumericButtonText;
+            button8.Click += GetNumericButtonText;
+            button9.Click += GetNumericButtonText;
+            button0.Click += GetNumericButtonText;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             headerBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "Server=cis1.actx.edu;Database=project2;User Id=db2;Password = db20;";
+            connection.Open();
+
+            using (SqlCommand readAllCustomer = new SqlCommand())
+            {
+                readAllCustomer.CommandText = "select * dbo.Customers";
+
+                using (SqlDataReader reader = readAllCustomer.ExecuteReader())
+                {
+                    bool tryAgain = false;
+                    int customerID = 0;
+                    do
+                    {
+                        reader.Read();
+                        customerID = reader.GetInt32(0);
+                        if(CustomerID == customerID)
+                        {
+                            tryAgain = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("You're Customer ID does not match with the database");
+                        }
+                    }
+                    while (tryAgain == true);
+                }
+            }
+            DateTime reservedDate = DateTime.Now;
+
+            using (SqlCommand readAllReservations = new SqlCommand())
+            {
+                readAllReservations.CommandText = "select * dbo.Reservations";
+
+                using (SqlDataReader reader = readAllReservations.ExecuteReader())
+                {
+                    reader.Read();
+                    bool tryAgain = false;
+                    int customerID = 0;
+                    do
+                    {
+                        // searching for a reservation
+                        customerID = reader.GetInt32(5);
+                        if(customerID == CustomerID)
+                        {
+                            // it has found a reservation, now have to compare times to see if they are late
+                            // or early
+                            reservedDate = reader.GetDateTime(3);
+                        }
+                    }
+                    while (tryAgain == true);
+                }
+            }
+
+            startTime = DateTime.Now;
+            
+            if (startTime.Subtract(reservedDate) <= TimeSpan.FromMinutes(30))
+            {
+                //less than 30 minutes have passed
+                if (startTime.Subtract(reservedDate) >= TimeSpan.FromMinutes(0))
+                {
+                    // it is after the time has started
+
+                }
+                else
+                {
+                    lbl1.Text = "Your reserved period of time has not yet started, please get off the elevator and wait for the specified time";
+                }
+            }
+            else
+            {
+                lbl1.Text = "Your reserved period of time has gone beyond 30 minutes, please click the button to go back to reservations or get off the elevator";
+                toReservations.Visible = true;
+            }
+        }
+
+        
+
+
+        private void GetNumericButtonText(object sender, EventArgs e)
+        {
+            Button theButtonClicked = (Button)sender;
+            int convertedNumber = 0;
+            runningTotal += theButtonClicked.Text;
+            if (int.TryParse(runningTotal, out convertedNumber))
+            {
+                CustomerID = convertedNumber;
+            }
+            else
+            {
+                MessageBox.Show("");
+            }
+        }
+
+        private void toReservations_Click(object sender, EventArgs e)
+        {
+            TeamBWindowsFormViewReservationPage.ViewandModifyReservationsFrm reservationPage = new TeamBWindowsFormViewReservationPage.ViewandModifyReservationsFrm();
+            reservationPage.Activate();
+            reservationPage.Visible = true;
+            this.Visible = false;
         }
     }
 }
